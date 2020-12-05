123c432762585d5555fe9634289aa80b0bd1dc2a
João  Soares <joaosoares@MBP-de-Joao.lan>
2020-12-05 23:41:15 +0000

    t8

---
+++ TPCs/T8/public/imagens.js
@@ -0,0 +1,38 @@
+
+function showImage(name, type){
+    const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg']
+
+    if(validImageTypes.includes(type))
+        var ficheiro = '<img src="/fileStore/' + name + '" width="80%"/>'
+    else 
+        var ficheiro = '<p>' + name + ', ' + type + '<p>'
+    
+    var fileObj = $(`
+        <div class="w3-row w3-margin">
+            <div class="w3-col s6">
+                ${ficheiro}
+            </div>
+            <div class="w3-col s6 w3-border">
+                <p>Filename: ${name}</p>
+                <p>Mimetype: ${type}</p>
+            </div>
+        </div>
+    `)
+
+    var download = $('<div><a href="/files/download/' + name + '">Download</a></div>')
+    $('#display').empty()
+    $('#display').append(fileObj,download)
+    $('#display').modal()
+}
+
+function addFile(){
+    $('#insert').append(`<div class="w3-row">
+                <div class="w3-col s3 w3-margin-top">
+                    <label class="w3-text-teal">&#8203;</label>
+                </div>
+                    <div class="w3-col s9 w3-border w3-margin-top">
+                        <input class="w3-input w3-border w3-light-grey" type="file" name="myFile">
+                    </div>
+                </div>`)
+}
+
--- TPCs/T8/server.js
+++ TPCs/T8/server.js
@@ -1,22 +1,75 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var templates = require('./html-templates')
+var jsonFile = require('jsonfile')
+var logger = require('morgan')
+var fs = require('fs')
+
+
+var multer = require('multer')
+var upload = multer({dest: './uploads'})
 
 var app = express()
 
-app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(logger('dev'))
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+
 app.use(bodyParser.json())
 
-app.use(function(req,res,next) {
+app.use(express.static('./public'))
+
+app.use( (req,res,next) => {
     console.log(JSON.stringify(req.body))
     next()
 })
 
-app.get('*', function(req,res){
-    res.send('Recebi um Get...')
+app.get('/', (req,res) => {
+    var d = new Date().toISOString().substr(0,16)
+    var files = jsonFile.readFileSync('./dbFiles.json')
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    res.write(templates.fileList(files,d))
+    res.end()
 })
 
-app.post('*', function(req,res){
-    res.send('Recebi um Post...')
+app.get('/files/upload',function(req,res){
+    var d = new Date().toISOString().substr(0,16)
+    res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
+    res.write(templates.fileForm(d))
+    res.end()
 })
 
-app.listen(7700, () => console.log('Servidor à escuta na porta 7700...'))
+app.get('/files/download/:fname', function(req,res){
+    res.download(__dirname + '/public/fileStore/' + req.params.fname)      
+})
+
+app.post('/files', upload.array('myFile'), function(req,res){
+    var files = jsonFile.readFileSync('./dbFiles.json')
+    var d = new Date().toISOString().substr(0,16)
+
+    for(var i = 0; i < req.files.length; i++){
+        let oldPath = __dirname + '/' + req.files[i].path
+        let newPath = __dirname + '/public/fileStore/' + req.files[i].originalname
+    
+        fs.rename(oldPath,newPath, function(err){
+            if(err) throw err
+        })
+        
+        
+        files.push(
+            {
+                date: d,
+                name: req.files[i].originalname,
+                size: req.files[i].size,
+                mimetype: req.files[i].mimetype,
+                desc: req.body.desc
+            }
+        )
+    }
+    jsonFile.writeFileSync('./dbFiles.json', files)
+    res.redirect('/')
+})
+
+app.listen(7701, () => console.log('Servidor a escuta porta 7701...'))
